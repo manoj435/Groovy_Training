78def49bcd34386003a84b8f06ad2a9c124efd26
MANOJ <MANOJ@DESKTOP-CJVK58N>
2020-03-04 00:50:21 +0530

    DataTypes

---
+++ DataTypes/src/com/test/demo/DataTypes.groovy
@@ -0,0 +1,55 @@
+package com.test.demo
+
+/*
+ * Variables are used to store information
+ * This information can be of different type (eg : Boolean, String, Number)
+ */
+
+//byte
+byte b = 10
+println b
+println Byte.MIN_VALUE
+println Byte.MAX_VALUE
+println "=============="
+//short
+int s = 100
+println s
+println Short.MIN_VALUE
+println Short.MAX_VALUE
+println "==============="
+//int
+int i = 1000
+println i
+println Integer.MIN_VALUE
+println Integer.MAX_VALUE
+println "================"
+//long
+long l = 10000
+println l
+println Long.MIN_VALUE
+println Long.MAX_VALUE
+println "================"
+//float
+float f = 100.002F
+println f
+println Float.MIN_VALUE
+println Float.MAX_VALUE
+println "==============="
+//double
+double d = 1000.0002D
+println d
+println Double.MIN_VALUE
+println Double.MAX_VALUE
+println "================"
+//char
+char c = "A"
+println c
+println "==============="
+//Boolean
+boolean flag = "true"
+println flag
+println "================"
+//String
+String str = "groovy"
+println "================"
+println str
